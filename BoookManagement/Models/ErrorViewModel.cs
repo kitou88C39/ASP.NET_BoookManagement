56ed01ad53a36f315186543923f8b3f38e6e3b43
kitou88C39 <kanfutrooper@icloud.com>
2023-10-21 16:08:35 +0900

    first commit

---
+++ BoookManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BoookManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
